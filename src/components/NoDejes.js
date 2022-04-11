29adf5144d90cf4f793b871aebc72794f1c9efe5
Armando <amezal01@hotmail.com>
2022-04-11 16:56:22 -0600

    No Dejes section added

---
+++ src/components/NoDejes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './NoDejes.scss';
+
+const NoDejes = () => {
+  return (
+    <section className="no-dejes">
+      <div className="content content1">
+        <h2>No dejes que el marketing digital de tu negocio se te salga de las manos</h2>
+      </div>
+      <div className="content content2">
+        <p>
+          Hacer marketing hoy, no es lo mismo a lo que era hace 5 años.
+          Han surgido nuevas plaformas, nuevas funcionalidades, nuevas formas
+          de conectar. Hacer que el negocio funcione y seguirle el paso a
+          todas estas actualizaciones es una tarea agotadora.
+          Por eso quiero ayudarte.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default NoDejes;
