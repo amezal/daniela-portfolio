4d8fad87119f46db00fa3a5d46434ab6d232e602
Armando <amezal01@hotmail.com>
2022-07-16 19:53:03 -0600

    latest post banner

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Layout from '/src/components/Layout'
+import Banner from '/src/components/BannerBlog'
 import Seo from '/src/components/Seo'
 import { graphql, Link } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import './blog.css'
+import './blog.scss'
+
 
 function blog({ data }) {
 
@@ -15,36 +17,41 @@ function blog({ data }) {
   }
 
   const posts = data.allWpPost.nodes;
+  //TODO: hacer un query diferente para el primer post
+  const latestPost = posts[0];
 
   return (
     <Layout>
       <Seo title="Blog" />
       <main className="blog">
+        {
+          data &&
+          <>
+            <Banner post={latestPost} shortenString={shortenString} />
+            <div className="posts">
+              {posts.filter((p, i) => i > 0).map(post => (
+                <div key={post.id} className="article blog-article">
 
-        <h1>Posts</h1>
+                  <Link className="readmore" to={`/blog/${post.slug}`}>
+                    <GatsbyImage
+                      className="featured-img-wrapper"
+                      imgClassName="featured-img"
+                      image={getImage(post.featuredImage.node.localFile)}
+                      width={50}
+                      height={50}
+                      alt={post.title} />
+                  </Link>
 
-        <div className="posts">
-          {posts.map(post => (
-            <div key={post.id} className="article blog-article">
-
-              <Link className="readmore" to={`/blog/${post.slug}`}>
-                <GatsbyImage
-                  className="featured-img-wrapper"
-                  imgClassName="featured-img"
-                  image={getImage(post.featuredImage.node.localFile)}
-                  width={50}
-                  height={50}
-                  alt={post.title} />
-              </Link>
-
-              <Link className="readmore" to={`/blog/${post.slug}`}>
-                <h3>{post.title}</h3>
-              </Link>
-              <p dangerouslySetInnerHTML={{ __html: `${shortenString(post.excerpt)}` }}></p>
-              <Link className="readmore" to={`/blog/${post.slug}`}>Leer más</Link>
+                  <Link className="readmore" to={`/blog/${post.slug}`}>
+                    <h3>{post.title}</h3>
+                  </Link>
+                  <p dangerouslySetInnerHTML={{ __html: `${shortenString(post.excerpt)}` }}></p>
+                  <Link className="readmore" to={`/blog/${post.slug}`}>Leer más</Link>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </>
+        }
 
       </main>
     </Layout>
@@ -53,7 +60,7 @@ function blog({ data }) {
 
 export const pageQuery = graphql`
   query {
-    allWpPost(sort: { fields: [date] }) {
+    allWpPost(sort: { fields: [date], order: DESC }) {
       nodes {
         title
         excerpt
